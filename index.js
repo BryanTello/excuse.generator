f954c34006aab8df63c0a256a378fa7f69af9869
BryanTello <bryanbat65@gmail.com>
2021-07-19 08:05:56 +0000

    gen add

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+
+    document.querySelector("#btn"). addEventListener("click",
+    () =>{
+        document.querySelector("#the-excuse").innerHTML = generateExcuse();
+    });
+    
+    
+    console.log("Hello Rigo frem the console!");
+};
+let generateExcuse = () => {
+
+    let pronoun = [" A", " The" ];
+    let subject = [" jogger", " racoon", " dog", " driver", " comedian", " pincone" ];
+    let action = [" took my", " threw my", " yelled at my", " stole my", " bit my" ];
+    let possetion = [" homework", " toe", " car", " shoe" ]
+    let where = [" on the street", " in my house", " in my driveway" ];
+
+    let pronounIndex = Math.floor(Math.random() * pronoun.length);
+    let subjectIndex = Math.floor(Math.random() * subject.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let possetionIndex = Math.floor(Math.random() * possetion.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+
+    return(pronoun[pronounIndex].concat(action[actionIndex], subject[subjectIndex], possetion[possetionIndex], where[whereIndex]));
+}
